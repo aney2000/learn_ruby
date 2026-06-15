a911dcc7c14cc9903f0da657a497c35d8403d973
Dragan Andrei <aney2000@yahoo.com>
2026-06-15 19:23:53 +0300

    I wanted to know what's the best way of pig latin using regex

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,25 +1,3 @@
 def translate(text)
-  vocale = ["a", "e", "i", "o", "u"]
-  ay = "ay"
-  
-  cuvinte = text.split
-  cuvinte_traduse = []
-  
-  cuvinte.each do |cuvant|
-    if vocale.include?(cuvant[0])
-      cuvinte_traduse << cuvant + ay
-    else
-      while !vocale.include?(cuvant[0])
-        
-        if cuvant[0..1] == "qu"
-          cuvant = cuvant[2..-1] + "qu"
-        else
-          cuvant = cuvant[1..-1] + cuvant[0]
-        end
-        
-      end
-      cuvinte_traduse << cuvant + ay
-    end
-  end
-  cuvinte_traduse.join(" ")
+  text.gsub(/\b([^aeiou\s]*qu|[^aeiou\s]+)?([aeiou]\w*)/i, '\2\1ay')
 end
