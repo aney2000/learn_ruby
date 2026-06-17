f5b58a0dc607fd8b255e4a2f462be7b3794fcf02
Dragan Andrei <aney2000@yahoo.com>
2026-06-17 18:14:19 +0300

    refactor pig_latin with regex

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,25 +1,18 @@
 def translate(text)
-  vowels = ["a", "e", "i", "o", "u"]
   ay = "ay"
   
-  words = text.split
-  words_translated = []
-  
-  words.each do |word|
-    if vowels.include?(word[0])
-      words_translated << word + ay
+  text.split.map do |word|
+    if word[0] =~ /[aeiou]/i
+      word + ay
     else
-      while !vowels.include?(word[0])
-        
+      while word[0] =~ /[^aeiou]/i
         if word[0..1] == "qu"
           word = word[2..-1] + "qu"
         else
           word = word[1..-1] + word[0]
         end
-        
       end
-      words_translated << word + ay
+      word + ay
     end
-  end
-  words_translated.join(" ")
+  end.join(" ")
 end
